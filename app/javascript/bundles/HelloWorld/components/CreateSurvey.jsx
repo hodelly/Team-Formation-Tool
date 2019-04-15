d46d1f39ba57e302632e97d9447cfb26a7476a02
Morgan Sorbaro <msorbaro@gmail.com>
2019-04-14 20:51:51 -0400

    adding survey.jschanges

--- app/javascript/bundles/HelloWorld/components/CreateSurvey.jsx
+++ app/javascript/bundles/HelloWorld/components/CreateSurvey.jsx
@@ -1,6 +1,8 @@
 //import PropTypes from 'prop-types';
 import React from 'react';
 import MultipleChoiceQs from './MultipleChoiceQs'
+import * as Survey from "survey-react";
+import "survey-react/survey.css";
 
 export default class CreateSurvey extends React.Component {
   constructor(props) {
@@ -11,10 +13,15 @@ export default class CreateSurvey extends React.Component {
       questionAnswerMap: Map<Question, QuestionAnswers>
       Size: size of the map right now
     */
-    this.state={titleValue:"", answersValues: "", questionAnswerMap: new Map(), size:0};
+    this.state={titleValue:"", answersValues: "", surveyValue:"", surveyjs: null, inPreview:false};
     this.handleTitleChange = this.handleTitleChange.bind(this); //method to handle changes when typing in input bar for question
     this.handleValuesChange = this.handleValuesChange.bind(this); //method to handle changes when typing in input for ansnwers
-    this.handleSubmit = this.handleSubmit.bind(this); //method for when the submit button is clicked
+    this.handleSurveyChange = this.handleSurveyChange.bind(this); //method to handle changes when typing in input for ansnwers
+    this.handleSubmitCheckBox = this.handleSubmitCheckBox.bind(this); //method for when the submit button is clicked
+    this.handleSubmitRadio = this.handleSubmitRadio.bind(this); //method for when the submit button is clicked
+    this.handlePreview = this.handlePreview.bind(this); //method for when the submit button is clicked
+    this.onComplete = this.onComplete.bind(this); //method to handle changes when typing in input bar for question
+
   }
 
   // when the title is changed we update the titleValue state
@@ -23,20 +30,69 @@ export default class CreateSurvey extends React.Component {
   }
 
   // when the answer value inputs are changed then update the value state variable
+  handleSurveyChange(event) {
+    this.setState({surveyValue: event.target.value});
+  }
+
   handleValuesChange(event) {
     this.setState({answersValues: event.target.value});
   }
 
+  handlePreview(event) {
+    this.setState({inPreview: !this.state.inPreview});
+  }
+
+
+
   // when the submit button is clicked we add the question and the answers (split into an array) to the map
   // also update the size value for null check later
-  handleSubmit() {
-    this.state.questionAnswerMap[this.state.titleValue] = this.state.answersValues.split(",");
-    this.setState({size: this.state.size+1})
+  handleSubmitCheckBox() {
+    if(this.state.surveyjs!=null){
+      var surveyData = this.state.surveyjs;
+      var newQAdded = { type: "checkbox", choices: this.state.answersValues.split(","), isRequired: true, title: this.state.titleValue };
+      surveyData.pages[0].questions.push(newQAdded);
+      this.setState({surveyjs: surveyData});
+    }
+    else {
+      var surveyData = { title: this.state.surveyValue, pages: [
+        { name:"page1", questions: [
+          { type: "checkbox", choices: this.state.answersValues.split(","), isRequired: true, title: this.state.titleValue },
+        ]},
+      ]
+    };
+    this.setState({surveyjs: surveyData})
+    }
   }
+
+  handleSubmitRadio() {
+    if(this.state.surveyjs!=null){
+      var surveyData = this.state.surveyjs;
+      var newQAdded = { type: "radiogroup", choices: this.state.answersValues.split(","), isRequired: true, title: this.state.titleValue };
+      surveyData.pages[0].questions.push(newQAdded);
+      this.setState({surveyjs: surveyData});
+    }
+    else {
+      var surveyData = { title: this.state.surveyValue, pages: [
+        { name:"page1", questions: [
+          { type: "radiogroup", choices: this.state.answersValues.split(","), isRequired: true, title: this.state.titleValue },
+        ]},
+      ]
+    };
+    this.setState({surveyjs: surveyData})
+    }
+  }
+
+
+  onComplete(survey, options) {
+   //Write survey results into database
+   console.log("Survey results: " + JSON.stringify(survey.data));
+  }
+
 
   render() {
     //going to be the variable that contains all the multiple choice questions  created
     let questions = null;
+    console.log(this.state.surveyjs);
     if(this.state.size > 0){ //checks to make sure we have thigns in the map
       // turning all the keys into a list and looping through them
        questions = Object.keys(this.state.questionAnswerMap).map(question => {
@@ -46,20 +102,41 @@ export default class CreateSurvey extends React.Component {
       })
     }
 
+
    //individual question HTML that is returned
+   if(this.state.inPreview){
+     return(
+       <div>
+        <h1> Survey preview bellow </h1>
+         <script src="https://surveyjs.azureedge.net/1.0.79/survey.react.min.js"></script>
+         <link href="https://surveyjs.azureedge.net/1.0.79/survey.css" type="text/css" rel="stylesheet" />
+         <Survey.Survey json={this.state.surveyjs} onComplete={this.onComplete}/>
+      </div>
+     );
+   }
+   else{
     return (
       <div>
         <h3>Creating a Student Survey</h3>
-        <h1>Multiple Choice Questions </h1>
         <form onSubmit={this.handleSubmit}>
+          <label>Survey Title: <input type="text" value={this.state.surveyValue} onChange={this.handleSurveyChange} /></label>
           <label>Question Title: <input type="text" value={this.state.titleValue} onChange={this.handleTitleChange} /></label>
           <label>Options: <input type="text" value={this.state.answersValues} onChange={this.handleValuesChange} /></label>
         </form>
-        <button  type='button' onClick={this.handleSubmit}>
-          Submit
+        <div>
+        <button  type='button' onClick={this.handleSubmitCheckBox}>
+          Add CheckBox Q
+        </button>
+        <button  type='button' onClick={this.handleSubmitRadio}>
+          Add Radio Q
+        </button>
+        </div>
+        <button  type='button' onClick={this.handlePreview}>
+          Preview Survey
         </button>
         {questions}
       </div>
     );
   }
+}
 }
