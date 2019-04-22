032494445cba80ddf12da8a6c155dcbfeb0e8726
Morgan Sorbaro <msorbaro@gmail.com>
2019-04-21 23:00:56 -0400

    adding linter now it works and changes to keep linter happy because it was mad at me for so manny things

--- app/javascript/bundles/HelloWorld/components/CreateSurvey.jsx
+++ app/javascript/bundles/HelloWorld/components/CreateSurvey.jsx
@@ -1,133 +1,137 @@
-//import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import React from 'react';
-import * as Survey from "survey-react";
-import "survey-react/survey.css";
-import InputSurveyTitle from "./InputSurveyTitle"
-import AddQuestionToSurvey from "./AddQuestionToSurvey"
-import Question from "./Question"
+import * as Survey from 'survey-react';
+import 'survey-react/survey.css';
 import { Map } from 'immutable';
+import InputSurveyTitle from './InputSurveyTitle';
+import AddQuestionToSurvey from './AddQuestionToSurvey';
+import Question from './Question';
 
 
 export default class CreateSurvey extends React.Component {
   constructor(props) {
     super(props);
-    this.state={surveyValue:"", surveyjs: null, inPreview: false, titleStep: true, justAddedQStep: false, questionMap: new Map(), id:0};
+    this.state = {
+      surveyValue: '', surveyjs: null, inPreview: false, titleStep: true, justAddedQStep: false, questionMap: new Map(), id: 0,
+    };
     this.handleSurveyChange = this.handleSurveyChange.bind(this);
     this.handleSurveyTitleChange = this.handleSurveyTitleChange.bind(this);
     this.handlePreview = this.handlePreview.bind(this);
-    this.onComplete = this.onComplete.bind(this);
     this.handleNewQuestion = this.handleNewQuestion.bind(this);
     this.handleAddNewQuestion = this.handleAddNewQuestion.bind(this);
     this.deleteQuestion = this.deleteQuestion.bind(this);
     this.updateQuestion = this.updateQuestion.bind(this);
+  }
 
+  onComplete = (survey, options) => {
+    // Write survey results into database
+    console.log(`Survey results: ${JSON.stringify(survey.data)}`);
   }
 
   handleSurveyChange(event) {
-    this.setState({surveyValue: event.target.value});
+    this.setState({ surveyValue: event.target.value });
   }
 
-  deleteQuestion(id){
+  deleteQuestion(id) {
     this.setState(prevState => ({
       questionMap: prevState.questionMap.delete(id),
     }));
   }
 
-  updateQuestion(id, fields){
+  updateQuestion(id, fields) {
     this.setState(prevState => ({
       questionMap: prevState.questionMap.update(id, (q) => { return Object.assign({}, q, fields); }),
     }));
   }
 
   handleSurveyTitleChange(title) {
-    this.setState({surveyValue: title});
-    this.setState({titleStep: false});
-    var surveyData = { title: title, pages: [
-      { name:"page1", questions: []},
-    ]};
-    this.setState({surveyjs: surveyData})
+    this.setState({ surveyValue: title });
+    this.setState({ titleStep: false });
+    const surveyData = {
+      title,
+      pages: [
+        { name: 'page1', questions: [] },
+      ],
+    };
+    this.setState({ surveyjs: surveyData });
   }
 
   handlePreview(event) {
-    this.setState({inPreview: !this.state.inPreview});
+    // this.setState({ inPreview: !this.state.inPreview });
+    this.setState(prevState => ({ inPreview: !prevState.inPreview }));
   }
 
   handleAddNewQuestion(event) {
-    this.setState({justAddedQStep: !this.state.justAddedQStep});
+    // this.setState({ justAddedQStep: !this.state.justAddedQStep });
+    this.setState(prevState => ({ justAddedQStep: !prevState.justAddedQStep }));
   }
 
 
-
-  handleNewQuestion(newQuestion){
-    var surveyData = this.state.surveyjs;
-    surveyData.pages[0].questions.push(newQuestion);
+  handleNewQuestion(newQuestion) {
+    // const surveyData = this.state.surveyjs;
+    // surveyData.pages[0].questions.push(newQuestion);
     this.setState(prevState => ({
-      questionMap: prevState.questionMap.set(prevState.id, Object.assign({}, this.state.questionMap, newQuestion)),
+      questionMap: prevState.questionMap.set(prevState.id, Object.assign({}, prevState.questionMap, newQuestion)),
     }));
-    var newid= this.state.id;
-    newid = newid+1;
-    this.setState({surveyjs: surveyData, justAddedQStep: !this.state.justAddedQStep, id: newid});
-  }
+    this.setState(prevState => ({ id: prevState.id + 1 }));
+    this.setState(prevState => ({ surveyjs: prevState.surveyjs.pages[0].questions.push(newQuestion), justAddedQStep: !prevState.justAddedQStep }));
 
-
-  onComplete(survey, options) {
-   //Write survey results into database
-   console.log("Survey results: " + JSON.stringify(survey.data));
+  //  this.setState({ surveyjs: surveyData, justAddedQStep: !this.state.justAddedQStep, id: newid });
   }
 
 
   render() {
-  console.log(this.state);
-   if(this.state.inPreview){
-     return(
-       <div>
-        <h1> Survey preview bellow </h1>
-         <script src="https://surveyjs.azureedge.net/1.0.79/survey.react.min.js"></script>
-         <link href="https://surveyjs.azureedge.net/1.0.79/survey.css" type="text/css" rel="stylesheet" />
-         <Survey.Survey json={this.state.surveyjs} onComplete={this.onComplete}/>
-      </div>
-     );
-   }
-
-   else{
-     var savedQuestions = this.state.questionMap.entrySeq().map(([id, question]) => {
+    console.log(this.state);
+    if (this.state.inPreview) {
       return (
-        <Question
-          title={question.title}
-          choices={question.choices}
-          isRequired={question.isRequired}
-          type={question.type}
-          weight={question.weight}
-          id={id}
-          deleteQuestion={this.deleteQuestion}
-          updateQuestion={this.updateQuestion}
-          />
+        <div>
+          <h1> Survey preview bellow </h1>
+          <script src="https://surveyjs.azureedge.net/1.0.79/survey.react.min.js" />
+          <link href="https://surveyjs.azureedge.net/1.0.79/survey.css" type="text/css" rel="stylesheet" />
+          <Survey.Survey json={this.state.surveyjs} onComplete={this.onComplete} />
+        </div>
       );
+    } else {
+      const savedQuestions = this.state.questionMap.entrySeq().map(([id, question]) => {
+        return (
+          <Question
+            title={question.title}
+            choices={question.choices}
+            isRequired={question.isRequired}
+            type={question.type}
+            weight={question.weight}
+            id={id}
+            deleteQuestion={this.deleteQuestion}
+            updateQuestion={this.updateQuestion}
+          />
+        );
+      });
 
-     });
-
-     var addButton = (this.state.justAddedQStep) ?
-     <AddQuestionToSurvey handleNewQuestion={this.handleNewQuestion} /> :
-     (this.state.titleStep) ? null :
-     (
-       <div>
-       <button  type='button' onClick={this.handleAddNewQuestion}>
+      let addButton = null;
+      if (this.state.justAddedQStep) {
+        addButton = <AddQuestionToSurvey handleNewQuestion={this.handleNewQuestion} />;
+      } else if (!this.state.titleStep) {
+        addButton = (
+          <div>
+            <button type="button" onClick={this.handleAddNewQuestion}>
          Add A Question
-       </button>
-       <button  type='button' onClick={this.handlePreview}>
+            </button>
+            <button type="button" onClick={this.handlePreview}>
                Preview Survey
-       </button>
-       </div>
-     );
+            </button>
+          </div>
+        );
+      }
 
-    return (
-      <div>
-        <h3>Creating a Student Survey</h3>
-        <InputSurveyTitle handleSurveyTitleChange={this.handleSurveyTitleChange}/>
-        {savedQuestions}
-        {addButton}
-      </div>
-    );
+
+      return (
+        <div>
+          <h3>Creating a Student Survey</h3>
+          <InputSurveyTitle handleSurveyTitleChange={this.handleSurveyTitleChange} />
+          {savedQuestions}
+          {addButton}
+        </div>
+      );
+    }
   }
-}
 }
--- app/javascript/bundles/HelloWorld/components/HelloWorld.jsx
+++ app/javascript/bundles/HelloWorld/components/HelloWorld.jsx
@@ -1,16 +1,14 @@
-//import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import React from 'react';
 import CreateSurvey from './CreateSurvey';
 
-//The HelloWorld class used to have the starter code before and now it will be the basis of our code
+// The HelloWorld class used to have the starter code before and now it will be the basis of our code
 export default class HelloWorld extends React.Component {
-
   constructor(props) {
     super(props);
     // How to set initial state in ES6 class syntax
     // https://reactjs.org/docs/state-and-lifecycle.html#adding-local-state-to-a-class
-    this.state = { viewSurveys: false, createSurvey: false };
-
+    this.state = { createSurvey: false };
   }
 
   render() {
@@ -18,20 +16,17 @@ export default class HelloWorld extends React.Component {
     Right now this is a "create survey component". We can add other things like review groups, finalize groups etc.
     Create survey is really just for adding a new question right now
     */
-    var welcome = (
+    const welcome = (
       <div>
-      <h3> Hello Professor! Welcome to your Survey Page </h3>
-      <button  type='button' onClick={() => {this.setState({createSurvey: true})}}>
+        <h3> Hello Professor! Welcome to your Survey Page </h3>
+        <button type="button" onClick={() => { this.setState({ createSurvey: true }); }}>
         Add A Survey
-      </button>
-      <button  type='button' onClick={() => {this.setState({viewSurveys: true})}}>
-        View Surveys
-      </button>
+        </button>
       </div>
     );
-    var display = welcome;
-    if(this.state.createSurvey=== true){
-      display=(<CreateSurvey />);
+    let display = welcome;
+    if (this.state.createSurvey === true) {
+      display = (<CreateSurvey />);
     }
     return (
       <div>
