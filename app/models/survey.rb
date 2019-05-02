018fc087dacfdfc4aa980c137071012875f0d3e0
annie ke <annieke8@gmail.com>
2019-05-01 20:10:26 -0400

    data model changes with notes from mo

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,12 +1,12 @@
 class Survey < ApplicationRecord
   validates :course_id, presence: true, length: { maximum: 32 }
   has_many :survey_questions
-  has_many :questions, :through => :survey_questions # TODO: what's hash syntax?
+  has_many :questions, through: :survey_questions
   serialize :sis_instructor_id, presence: true
   validates :group_size, presence: true, numericality: { only_integer: true }
-  date :due_date, presence: true, # TODO: how to validate dates?
+  validates :due_date, presence: true, numericality: true
 
-  scope :for_instructor, -> (sis_instructor_id) {
+  scope :for_instructor, lambda do |sis_instructor_id|
     where(sis_instructor_id: sis_instructor_id) # TODO: what about a serialized id though
-  }
+  end
 end
--- app/models/survey_question.rb
+++ app/models/survey_question.rb
@@ -1,6 +1,6 @@
 class SurveyQuestion < ApplicationRecord
   belongs_to :survey
-  has_one :question 
+  has_one :question
   validates :weight, presence: true, numericality: true
   has_many :response
 end
