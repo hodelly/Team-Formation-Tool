a027ff869b05ffef51a46a6b89c264b4b90ef9c1
annie ke <annieke8@gmail.com>
2019-05-01 20:46:34 -0400

    added skeleton for survey route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       resource :sis_user, only: :show
       resources :surveys, only: [:create, :destroy]
+    end
+  end
   get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
