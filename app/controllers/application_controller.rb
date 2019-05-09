1606660a6475a698d5276fe27041e75fcf18f99b
Morgan Sorbaro <msorbaro@gmail.com>
2019-05-09 13:43:22 -0400

    morgans canvas branch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,33 @@ require 'oauth/request_proxy/action_controller_request'
 
 $oauth_key = "test"
 $oauth_secret = "secret"
+require 'net/https'
+
+
+#var certOptions = {
+#  key: fs.readFileSync(path.resolve('build/cert/server.key')),
+#  cert: fs.readFileSync(path.resolve('build/cert/server.crt'))
+#}
+
+#var app = express()
+
+#var server = https.createServer(certOptions, app).listen(443)
+
 class ApplicationController < ActionController::Base
-<<<<<<< HEAD
   ## Comes in here
   def get_canvas_auth
     ## comes in here
+
+  #  $certOptions = {
+    #   fs.readFileSync(path.resolve('build/cert/server.key'))
+  #     fs.readFileSync(path.resolve('build/cert/server.crt'))
+  #  }
+
+  h = Net::HTTP.new("localhost", 443)
+  h.use_ssl = true
+  h.ssl_version = :SSLv3
+  h.verify_mode = OpenSSL::SSL::VERIFY_PEER
+  h.get "3000"
 
     begin
       signature = OAuth::Signature.build(request, :consumer_secret => $oauth_secret)
@@ -24,16 +46,15 @@ class ApplicationController < ActionController::Base
       # make sure this is an assignment tool launch, not another type of launch.
       # only assignment tools support the outcome service, since only they appear
       # in the Canvas gradebook.
-      unless params['lis_outcome_service_url'] && params['lis_result_sourcedid']
-        return %{It looks like this LTI tool wasn't launched as an assignment, or you are trying to take it as a teacher rather than as a a student. Make sure to set up an external tool assignment as outlined <a target="_blank" href="https://github.com/instructure/lti_example">in the README</a> for this example.}
-      end
+      # unless params['lis_outcome_service_url'] && params['lis_result_sourcedid']
+      #  return %{It looks like this LTI tool wasn't launched as an assignment, or you are trying to take it as a teacher rather than as a a student. Make sure to set up an external tool assignment as outlined <a target="_blank" href="https://github.com/instructure/lti_example">in the README</a> for this example.}
+      # end
 
       %w(lis_outcome_service_url lis_result_sourcedid lis_person_name_full lis_person_contact_email_primary).each { |v| session[v] = params[v] }
 
-
+  end
       # canvas_auth = CanvasAuth.new
       # session[:canvas_auth_id] = canvas_auth.session_code
-=======
   protect_from_forgery with: :null_session
 
   # Authentication methods
@@ -55,6 +76,5 @@ class ApplicationController < ActionController::Base
       session[:return_url] = request.url
       redirect_to '/auth/cas'
     end
->>>>>>> 984b5b43880bb390d7982956e83084ad58101b44
   end
 end
