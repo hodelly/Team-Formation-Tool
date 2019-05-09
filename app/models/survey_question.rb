b2589aa8761b3557dd8b8164825d3182086af0b6
annie ke <annieke8@gmail.com>
2019-05-09 13:43:52 -0400

    changed responses to plural in survey_question model

--- app/models/survey_question.rb
+++ app/models/survey_question.rb
@@ -2,5 +2,5 @@ class SurveyQuestion < ApplicationRecord
   belongs_to :survey
   has_one :question
   validates :weight, presence: true, numericality: true
-  has_many :response
+  has_many :responses
 end
