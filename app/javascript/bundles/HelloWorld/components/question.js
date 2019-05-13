c444cf9e06f109ee38f21742d6cd94eadec98336
Janvi Kalra <janvi.kalra.21@dartmouth.edu>
2019-05-13 05:43:55 -0400

    styling options, distributions, question components

--- app/javascript/bundles/HelloWorld/components/question.js
+++ app/javascript/bundles/HelloWorld/components/question.js
@@ -55,7 +55,6 @@ export default class Question extends React.Component {
 
   render() {
     let options;
-    // const array = ['janvi', 'kalra', 'options'];
     if (this.state.questionType === 'radiogroup' || this.state.questionType === 'checkbox' || this.state.questionType === 'dropdown') {
       options = <Options questionID={this.state.questionID} updateOptions={this.props.updateOptions} choicesList={this.state.choices} />;
     } else {
