aaf778bd6256268bdf8439aff805dabc26344456
annie ke <annieke8@gmail.com>
2019-05-15 20:22:33 -0400

    added column of question title to question

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,6 +7,7 @@ class Question < ApplicationRecord
   QUESTION_TYPES = [
     MULTIPLE_CHOICE, CHECKBOX, SHORT_ANSWER, PARAGRAPH
   ].freeze
+  validates :question_title, presence: true
   serialize :response_values, JSON # for multiple choice and checkbox
   validates :question_type, presence: true, length: { maximum: 32 }, inclusion: { in: QUESTION_TYPES }
   validates :is_default, inclusion: { in: [true, false] }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_15_024243) do
+ActiveRecord::Schema.define(version: 2019_05_16_002111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_05_15_024243) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "is_default"
+    t.string "question_title"
   end
 
   create_table "responses", force: :cascade do |t|
