5fcfcb0eb1b873af8312fd7185ce36d42fd5a311
annie ke <annieke8@gmail.com>
2019-05-22 20:51:00 -0400

    initialize survey responses route

---
+++ app/controllers/api/v1/survey_responses_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::SurveyResponsesController < ApplicationController
+    before_action :require_authentication
+  
+    def create
+      # submit a survey response (should only be used by students)
+    end
+  
+    def index
+      # render this specific student's survey responses (for all surveys)
+      # currently for debugging
+      render json: 'hi'
+    end
+  
+    def show
+      # load a specific survey response
+    end
+  end
