7012b9ded3936c930d88420f2cf3c7c76daf044f
annie ke <annieke8@gmail.com>
2019-05-23 14:18:49 -0400

    adding new response model and writing abilities

--- app/controllers/api/v1/survey_responses_controller.rb
+++ app/controllers/api/v1/survey_responses_controller.rb
@@ -3,6 +3,17 @@ class Api::V1::SurveyResponsesController < ApplicationController
   
     def create
       # submit a survey response (should only be used by students)
+      @sr = SurveyResponse.create_from_params(params[:responses])
+      @survey = Survey.find(params[:survey_id])
+      @survey.survey_responses << @sr
+
+      respond_to do |format|
+        if @sr.save
+          format.json { render json: @sr, status: :created }
+        else
+          format.json { render json: @sr.errors, status: :unprocessable_entity }
+        end
+      end
     end
   
     def index
@@ -12,6 +23,10 @@ class Api::V1::SurveyResponsesController < ApplicationController
     end
   
     def show
-      # load a specific survey response
+      @survey = Survey.find(params[:survey_id]) # should it be in the link?
+      @survey_response = @survey.survey_response.fine(curr_sis_user)
+      respond_to do |format|
+        format.json { render json: @survey_response.to_json(:include => [:responses]), status: :ok}
+      end
     end
   end
--- app/models/survey_response.rb
+++ app/models/survey_response.rb
@@ -3,4 +3,18 @@ class SurveyResponse < ApplicationRecord
     validates :sis_user_id, presence: true, length: { maximum: 32 }
     has_many :responses
     validates :is_submitted, inclusion: { in: [true, false] }
+
+    def self.create_from_params(params)
+      sr = new
+      # TODO change this!!
+      s.course_id = params[:course_id]
+      s.sis_instructor_id = params[:sis_instructor_id]
+      s.title = params[:title]
+      s.is_published = params[:is_published] == 'true'
+      s.description = params[:description] || ''
+      s.note_from_instructor = params[:note_from_instructor] || ''
+      s.group_size = params[:group_size].to_i
+      s.due_date = Time.at(params[:due_date].to_f/1000)
+      sr
+    end
   end
