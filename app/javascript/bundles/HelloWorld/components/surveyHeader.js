fe352a07c362a524aef3fd396b6697b919eed28d
Janvi Kalra <janvi.kalra.21@dartmouth.edu>
2019-05-28 22:34:24 -0400

    fixed sm bugs in questionPage and connected to API

--- app/javascript/bundles/HelloWorld/components/surveyHeader.js
+++ app/javascript/bundles/HelloWorld/components/surveyHeader.js
@@ -15,14 +15,26 @@ export default class SurveyHeader extends React.Component {
     };
   }
 
+  updateSurveyTitle = (event) => {
+    this.props.updateSurveyTitle(event.target.value);
+  }
+
+  updateSurveyDescription = (event) => {
+    this.props.updateSurveyDescription(event.target.value);
+  }
+
+  updateSurveyDueDate = (event) => {
+    this.props.updateSurveyDueDate(event.target.value);
+  }
+
 
   render() {
     // if continue button not clicked display buckets
     return (
       <div className="surveyHeader">
-        <input className="surveyTitle" type="text" placeholder="ENGS 21 Group Formation Survey" onClick={this.props.updateTitle} />
-        <input className="surveyInfo" type="text" placeholder="Survey Description" onClick={this.props.updateDescription} />
-        <input className="surveyInfo" type="text" placeholder="Survey due date" onClick={this.props.updateDueDate} />
+        <input className="surveyTitle" type="text" placeholder="ENGS 21 Group Formation Survey" value={this.props.surveyTitle} onChange={this.updateSurveyTitle} />
+        <input className="surveyInfo" type="text" placeholder="Survey Description" value={this.props.surveyDescription} onChange={this.updateSurveyDescription} />
+        <input className="surveyInfo" type="text" placeholder="Survey due date" value={this.props.surveyDueDate} onChange={this.updateSurveyDueDate} />
         <p> <span className="bold"> The group formation tool automatically retrieves this data from Canvas: </span> <br />
           <span className="italics"> Student First and Last Name <br />
           Student NetID <br />
